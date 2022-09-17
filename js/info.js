4e97ba6e6c0d7e889ec58529b358d1602bcdd985
fajkker <fedeparadelo@gmail.com>
2022-09-17 11:52:54 -0300

    New

---
+++ js/info.js
@@ -0,0 +1,43 @@
+const information = [
+    {
+        clase: 'Mago',
+        arma: 'Magia',
+        src: "https://fontawesome.com/icons/wand-magic?s=solid&f=classic",
+        intelecto: 20,
+        fuerza: 10,
+        constitucion: 15,
+        agilidad: 10
+    },
+    {
+        clase: 'Warrior',
+        arma: 'Maza',
+        intelecto: 25,
+        fuerza: 15,
+        constitucion: 10,
+        agilidad: 10
+    },
+    {
+        clase: 'Paladin',
+        arma: 'Espada',
+        intelecto: 10,
+        fuerza: 20,
+        constitucion: 30,
+        agilidad: 20
+    },
+    {
+        clase: 'Bardo',
+        arma: 'Musica',
+        intelecto: 10,
+        fuerza: 20,
+        constitucion: 30,
+        agilidad: 20
+    },
+    {
+        clase: 'Arquero',
+        arma: 'Arco',
+        intelecto: 10,
+        fuerza: 20,
+        constitucion: 30,
+        agilidad: 20
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,56 @@
+function lanzardados() {
+    let numero1 = getNumRand(1, 9);
+    let numero2 = getNumRand(1, 9);
+    let numero3 = getNumRand(1, 9);
+    let numero4 = getNumRand(1, 9);
+    let suma = numero1 + numero2 + numero3 + numero4;
+
+    $({ deg: 0 }).animate({ deg: 360 }, {
+        duration: 700,
+        step: function (now) {
+            var scale = (1 * now / 360);
+            $('#ImgNum1').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+            $('#ImgNum2').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+            $('#ImgNum3').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+            $('#ImgNum4').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+        }
+    });
+
+
+    document.getElementById("ImgNum1").src = "img/dungeon/" + numero1 + ".svg";
+    document.getElementById("ImgNum2").src = "img/dungeon/" + numero2 + ".svg";
+    document.getElementById("ImgNum3").src = "img/dungeon/" + numero3 + ".svg";
+    document.getElementById("ImgNum4").src = "img/dungeon/" + numero4 + ".svg";
+    document.getElementById("SumaDados").innerHTML = suma;
+    
+    $('#int').click(function () {
+        let int = suma;
+        console.log("Has sumado " + int + " en Inteligencia")
+        document.getElementById("int").innerHTML = suma;
+    });
+
+    $('#str').click(function () {
+        console.log("Has sumado " + suma + " en Fuerza")
+        document.getElementById("str").innerHTML = suma;
+    });
+
+    $('#const').click(function () {
+        console.log("Has sumado " + suma + " en Constitucion")
+        document.getElementById("const").innerHTML = suma;
+    });
+
+    $('#sab').click(function () {
+        console.log("Has sumado " + suma + " en Sabiduria")
+        document.getElementById("sab").innerHTML = suma;
+    });
+}
+
+
